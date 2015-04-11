9ac5513c41bd7913bd712795e4f8635a1e3490a0
Katarzyna Biernat <k.biernat@lgbs.pl>
2015-04-11 18:59:05 +0200

    Connected with phone - send random data

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
 __author__ = 'kbiernat'
 
-print ("My first Python code!")
+import threading
+from ComssServiceDevelopment.connectors.tcp.stream_connector import InputStreamConnector, OutputStreamConnector
+from ComssServiceDevelopment.service import Service, ServiceController #import modułów klasy bazowej Service oraz kontrolera usługi
+
+class MainService(Service):
+    def __init__(self): #"nie"konstruktor, inicjalizator obiektu usługi
+        super(MainService, self).__init__() #wywołanie metody inicjalizatora klasy nadrzędnej
+
+    def declare_outputs(self):
+        pass
+
+    def declare_inputs(self):
+        self.declare_input("audioInput", InputStreamConnector(self))
+
+    def run(self):
+        audio_input = self.get_input("audioInput") #obiekt interfejsu wejściowego
+        #video_output = self.get_output("videoOutput") #obiekt interfejsu wyjściowego
+
+        while self.running():
+            audio_data = audio_input.read()
+            print (audio_data)
+
+if __name__=="__main__":
+    sc = ServiceController(MainService, "main.json") #utworzenie obiektu kontrolera usługi
+    sc.start() #uruchomienie usługi
